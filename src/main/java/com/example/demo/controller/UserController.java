b0cba9f8346718951e4b6f84d933ddaffdcbcd57
ashishkumar9211 <ashishkumar9211@gmail.com>
2020-01-26 12:50:03 +0530

    Added new User

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -18,8 +18,10 @@ public class UserController {
 		List<User> lstUser = new ArrayList<>();
 		User user1 = new User("Test", "1", "test123");
 		User user2 = new User("James", "2", "james123");
+		User user3 = new User("Hello", "3", "hello123");
 		lstUser.add(user1);
 		lstUser.add(user2);
+		lstUser.add(user3);
 		return lstUser;
 	}
 	
